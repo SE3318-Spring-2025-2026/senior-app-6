ba24cb9e9911113f5565aaa08e65e10287d1cf3c
this-demir <23070006036@stu.yasar.edu.tr>
2026-04-10 20:49:43 +0300

    fix(test): wire SpringBootTest context

--- backend/src/test/java/com/senior/spm/SpmApplicationTests.java
+++ backend/src/test/java/com/senior/spm/SpmApplicationTests.java
@@ -1,8 +1,10 @@
 package com.senior.spm;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
+@SpringBootTest
 @TestPropertySource(locations = "classpath:test.properties")
 class SpmApplicationTests {
 
